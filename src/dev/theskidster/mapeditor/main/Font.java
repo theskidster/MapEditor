bbe816cf8570e60693846274d8ab8aeda08af63a
theskidster <theskidster@gmail.com>
2021-07-27 15:47:10 -0500

    added color class

---
+++ src/dev/theskidster/mapeditor/main/Font.java
@@ -0,0 +1,23 @@
+package dev.theskidster.mapeditor.main;
+
+import dev.theskidster.mapeditor.graphics.Color;
+
+/**
+ * Created: Jul 27, 2021
+ */
+
+/**
+ * @author J Hoffman
+ * @since  
+ */
+public class Font {
+
+    Font(String filename, int size) {
+        
+    }
+    
+    public void drawString(String text, int xPos, int yPos, Color color) {
+        
+    }
+    
+}
