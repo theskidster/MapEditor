507c1a1580b4d6f38f427ea1cd3c7a1677485b97
theskidster <theskidster@gmail.com>
2021-07-27 15:32:40 -0500

    added camera doc

--- src/dev/theskidster/mapeditor/main/Camera.java
+++ src/dev/theskidster/mapeditor/main/Camera.java
@@ -9,6 +9,9 @@ import org.joml.Vector3f;
  */
 
 /**
+ * Represents a camera that can freely traverse the scene using input from the 
+ * keyboard.
+ * 
  * @author J Hoffman
  * @since  0.0.0
  */
@@ -30,6 +33,13 @@ final class Camera {
     private final Matrix4f viewMatrix = new Matrix4f();
     private final Matrix4f projMatrix = new Matrix4f();
     
+    /**
+     * Applies whatever changes that occurred to the cameras projection matrix 
+     * between this frame and the last.
+     * 
+     * @param viewportWidth  the width (in pixels) of the viewport
+     * @param viewportHeight the height (in pixels) of the viewport
+     */
     void update(int viewportWidth, int viewportHeight) {
         projMatrix.setPerspective((float) Math.toRadians(60f), 
                                   (float) viewportWidth / viewportHeight, 
@@ -37,6 +47,11 @@ final class Camera {
                                   Float.POSITIVE_INFINITY);
     }
     
+    /**
+     * Renders the scene from the perspective of the camera.
+     * 
+     * @param sceneProgram the shader program used for rendering the scene
+     */
     void render(GLProgram sceneProgram) {
         viewMatrix.setLookAt(position, position.add(direction, tempVec1), up);
         
@@ -44,14 +59,30 @@ final class Camera {
         sceneProgram.setUniform("uProjection", false, projMatrix);
     }
     
+    /**
+     * Applies sensitivity preferences to the cameras directional changes.
+     * 
+     * @param currValue   the current position of the mouse cursor along an axis
+     * @param prevValue   the previous position of the mouse cursor along an 
+     *                    axis
+     * @param sensitivity the value used to dampen the final calculated output
+     * 
+     * @return a value denoting the intensity of the cameras look movement
+     */
     private float getChangeIntensity(double currValue, double prevValue, float sensitivity) {
         return (float) (currValue - prevValue) * sensitivity;
     }
     
-    public void setPosition(double xPos, double yPos) {
-        if(xPos != prevX || yPos != prevY) {
-            float speedX = getChangeIntensity(-xPos, -prevX, 0.017f);
-            float speedY = getChangeIntensity(-yPos, -prevY, 0.017f);
+    /**
+     * Sets the position of the camera.
+     * 
+     * @param cursorX the current x-coordinate of the mouse cursor
+     * @param cursorY the current y-coordinate of the mouse cursor
+     */
+    public void setPosition(double cursorX, double cursorY) {
+        if(cursorX != prevX || cursorY != prevY) {
+            float speedX = getChangeIntensity(-cursorX, -prevX, 0.017f);
+            float speedY = getChangeIntensity(-cursorY, -prevY, 0.017f);
             
             position.add(direction.cross(up, tempVec1).normalize().mul(speedX));
             
@@ -62,15 +93,21 @@ final class Camera {
             
             position.add(0, direction.cross(tempVec1, tempVec2).normalize().mul(speedY).y, 0);
             
-            prevX = xPos;
-            prevY = yPos;
+            prevX = cursorX;
+            prevY = cursorY;
         }
     }
     
-    public void setDirection(double xPos, double yPos) {
-        if(xPos != prevX || yPos != prevY) {
-            yaw   += getChangeIntensity(xPos, prevX, 0.35f);
-            pitch += getChangeIntensity(yPos, prevY, 0.35f);
+    /**
+     * Sets the current direction the camera will face.
+     * 
+     * @param cursorX the current x-coordinate of the mouse cursor
+     * @param cursorY the current y-coordinate of the mouse cursor
+     */
+    public void setDirection(double cursorX, double cursorY) {
+        if(cursorX != prevX || cursorY != prevY) {
+            yaw   += getChangeIntensity(cursorX, prevX, 0.35f);
+            pitch += getChangeIntensity(cursorY, prevY, 0.35f);
             
             if(pitch > 89f)  pitch = 89f;
             if(pitch < -89f) pitch = -89f;
@@ -79,11 +116,17 @@ final class Camera {
             direction.y = (float) Math.sin(Math.toRadians(pitch)) * -1;
             direction.z = (float) (Math.sin(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch)));
             
-            prevX = xPos;
-            prevY = yPos;
+            prevX = cursorX;
+            prevY = cursorY;
         }
     }
     
+    /**
+     * Moves the camera backwards and forwards along the current directions 
+     * it's facing.
+     * 
+     * @param speed the speed at which the camera will move
+     */
     public void dolly(float speed) {
         position.add(direction.mul(speed, tempVec1));
     }
