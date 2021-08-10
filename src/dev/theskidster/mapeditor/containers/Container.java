3628916b6a04067aa0830ea26bec1fa882a9748c
theskidster <theskidster@gmail.com>
2021-08-09 19:11:19 -0500

    added container resize ability

--- src/dev/theskidster/mapeditor/containers/Container.java
+++ src/dev/theskidster/mapeditor/containers/Container.java
@@ -24,8 +24,10 @@ public abstract class Container extends Control {
 
     private final float minWidth;
     private final float minHeight;
+    private float initialPosY;
     
     private boolean allowMove;
+    private boolean allowResize;
     private boolean collapsed;
     private boolean prevClicked;
     
@@ -37,7 +39,7 @@ public abstract class Container extends Control {
     private Color collapseColor = Color.UI_SLATE_GRAY;
     
     private final Rectangle[] buttons = new Rectangle[3];
-    private final Icon[] icons        = new Icon[3];
+    private final Icon[] icons        = new Icon[4];
     
     protected List<Control> controls;
     
@@ -50,7 +52,7 @@ public abstract class Container extends Control {
         titleBar   = new Rectangle(xPos, yPos + minHeight - 34, minWidth, 34);
         buttons[0] = new Rectangle((xPos + minWidth) - 34, titleBar.yPos, 34, 34);
         buttons[1] = new Rectangle((xPos + minWidth) - 68, titleBar.yPos, 34, 34);
-        buttons[2] = new Rectangle();
+        buttons[2] = new Rectangle((xPos + minWidth) - 15, yPos, 15, 15);
         
         icons[0] = new Icon(20, 20);
         icons[0].setSubImage(cellX, cellY);
@@ -62,16 +64,70 @@ public abstract class Container extends Control {
         icons[2] = new Icon(20, 20);
         icons[2].setSubImage(4, 5);
         icons[2].position.set(buttons[1].xPos + 7, buttons[1].yPos + 7);
+        
+        icons[3] = new Icon(20, 20);
+        icons[3].setSubImage(6, 5);
+        icons[3].setColor(Color.UI_LIGHT_GRAY);
+        icons[3].position.set(buttons[2].xPos - 5, buttons[2].yPos);
+    }
+    
+    private void configure() {
+        titleBar.xPos  = bounds.xPos;
+        titleBar.yPos  = bounds.yPos + bounds.height - 34;
+        titleBar.width = bounds.width;
+        
+        icons[0].position.set(bounds.xPos + 7, titleBar.yPos + 7);
+        
+        buttons[0].xPos = (titleBar.xPos + titleBar.width) - 34;
+        buttons[0].yPos = titleBar.yPos;
+        
+        buttons[1].xPos = (titleBar.xPos + titleBar.width) - 68;
+        buttons[1].yPos = titleBar.yPos;
+        
+        buttons[2].xPos = (bounds.xPos + bounds.width) - 15;
+        buttons[2].yPos = bounds.yPos;
+        
+        icons[1].position.set(buttons[0].xPos + 7, buttons[0].yPos + 7);
+        icons[2].position.set(buttons[1].xPos + 7, buttons[1].yPos + 7);
+        icons[3].position.set(buttons[2].xPos - 5, buttons[2].yPos);
+        
+        controls.forEach(control -> control.relocate(bounds.xPos, bounds.yPos));
     }
     
     protected abstract Command updateAdjunct(Mouse mouse);
     
     protected abstract void renderAdjunct(GLProgram uiProgram, Background background, Font font);
     
+    protected boolean controlHovered(Vector2f cursorPos) {
+        return controls.stream().anyMatch(control -> control.hovered(cursorPos));
+    }
+    
     @Override
     public Command update(Mouse mouse) {
-        if(bounds.width < minWidth) bounds.width = minWidth;
-        if(bounds.height < minHeight) bounds.height = minHeight;
+        if(mouse.clicked && !collapsed) {
+            if(buttons[2].contains(mouse.cursorPos) && !allowResize) {
+                allowResize = true;
+                initialPosY = bounds.yPos;
+            }
+            
+            if(allowResize) {
+                bounds.width  = mouse.cursorPos.x - bounds.xPos;
+                bounds.height += bounds.yPos - mouse.cursorPos.y;
+                
+                if(bounds.width < minWidth) bounds.width = minWidth;
+                
+                if(bounds.height < minHeight) {
+                    bounds.height = minHeight;
+                    bounds.yPos   = initialPosY;
+                } else {
+                    bounds.yPos   = mouse.cursorPos.y;
+                }
+                
+                configure();
+            }
+        } else {
+            allowResize = false;
+        }
         
         if(!buttons[0].contains(mouse.cursorPos) && !buttons[1].contains(mouse.cursorPos)) {
             if(mouse.clicked) {
@@ -118,7 +174,8 @@ public abstract class Container extends Control {
         
         prevClicked = mouse.clicked;
         
-        return updateAdjunct(mouse);
+        if(!collapsed) return updateAdjunct(mouse);
+        else           return null;
     }
     
     @Override
@@ -128,34 +185,17 @@ public abstract class Container extends Control {
         background.drawRectangle(buttons[0], closeColor, uiProgram);
         background.drawRectangle(buttons[1], collapseColor, uiProgram);
         
-        for(Icon icon : icons) icon.render(uiProgram);
+        for(int i = 0; i < icons.length; i++) {
+            if(i == 3) {
+                if(!collapsed) icons[i].render(uiProgram);
+            } else {
+                icons[i].render(uiProgram);
+            }
+        }
         
         font.drawString(title, titleBar.xPos + 34, titleBar.yPos + 10, Color.UI_WHITE, uiProgram);
         
         if(!collapsed) renderAdjunct(uiProgram, background, font);
-    }
-    
-    protected boolean controlHovered(Vector2f cursorPos) {
-        return controls.stream().anyMatch(control -> control.hovered(cursorPos));
-    }
-    
-    private void configure() {
-        titleBar.xPos  = bounds.xPos;
-        titleBar.yPos  = bounds.yPos + bounds.height - 34;
-        titleBar.width = bounds.width;
-        
-        icons[0].position.set(bounds.xPos + 7, titleBar.yPos + 7);
-        
-        buttons[0].xPos = (titleBar.xPos + titleBar.width) - 34;
-        buttons[0].yPos = titleBar.yPos;
-        
-        buttons[1].xPos = (titleBar.xPos + titleBar.width) - 68;
-        buttons[1].yPos = titleBar.yPos;
-        
-        icons[1].position.set(buttons[0].xPos + 7, buttons[0].yPos + 7);
-        icons[2].position.set(buttons[1].xPos + 7, buttons[1].yPos + 7);
-        
-        controls.forEach(control -> control.relocate(bounds.xPos, bounds.yPos));
     }
     
     @Override
